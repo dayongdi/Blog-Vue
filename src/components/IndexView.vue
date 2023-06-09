8767e6446e55b03ac37605be259dea1b12777255
LAPTOP-B8SHAVIK\1 <328069454@qq.com>
2023-06-09 18:05:44 +0800

    init

--- src/components/IndexView.vue
+++ src/components/IndexView.vue
@@ -1,11 +1,14 @@
 <template>
-	<my-nav></my-nav>
+	<div id="topDiv" class="text-center">
+		<my-nav></my-nav>
+		<div class="BigText">Blog</div>
+		<a>/*这里是DYD的测试*/</a>
+	</div>
+	
 </template>
 
 <script>
 import myNav from '../components/myNav.vue';
-
-	
 	export default{
 		name:"indexView",
 		components:{
@@ -15,4 +18,14 @@ import myNav from '../components/myNav.vue';
 </script>
 
 <style>
+	#topDiv{
+		height: 100%;
+		max-width: auto;
+		max-height: 100%;
+		background-image: url("/src/assets/topBack.jpg");
+	}
+	
+	.BigText{
+		font-size: 80px;
+	}
 </style>
--- src/components/myNav.vue
+++ src/components/myNav.vue
@@ -2,24 +2,54 @@
 	<v-app-bar>
 		<v-btn icon="$vuetify" id="toolbarTitle"></v-btn>
 		<v-spacer></v-spacer>
-		<div></div>
-		<v-btn class="d-none d-sm-flex">首页</v-btn>
-		<v-btn class="d-none d-sm-flex">关于</v-btn>
-		<v-btn class="d-none d-sm-flex">日志</v-btn>
-		<v-btn class="d-none d-sm-flex">留言板</v-btn>
-		<v-btn class="d-none d-sm-flex">待定</v-btn>
-		
-		
+		<v-btn class="d-none d-sm-flex myNavTitle">首页</v-btn>
+		<v-btn class="d-none d-sm-flex myNavTitle">关于</v-btn>
+		<v-btn class="d-none d-sm-flex myNavTitle">日志</v-btn>
+		<v-btn class="d-none d-sm-flex myNavTitle">留言板</v-btn>
+		<a class="d-none d-sm-flex myNavTitle"></a>
+		<v-btn
+			class="d-flex d-sm-none"
+            color="primary"
+			icon="$vuetify"
+            @click.stop="drawer = !drawer"
+          >
+        </v-btn>
 	</v-app-bar>
+	
+	<v-navigation-drawer v-model="drawer" location="right" temporary>
+		<v-list>
+			<v-list-item><v-btn class="dialogItem">首页</v-btn></v-list-item>
+			<v-list-item><v-btn class="dialogItem">关于</v-btn></v-list-item>
+			<v-list-item><v-btn class="dialogItem">日志</v-btn></v-list-item>
+			<v-list-item><v-btn class="dialogItem">留言板</v-btn></v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+	
+	
 	
 </template>
 
+
 <script>
-	
 	export default{
-		name:"MyNav"
+		name:"MyNav",
+		data: () => {
+			return{
+				drawer:null,
+			}
+		}
 	}
 </script>
 
 <style>
+.myNavTitle{
+	text-align: center;
+	width: 5%;
+	margin-left: 1%;
+	margin-right: 1%;
+}
+
+.dialogItem{
+	width: 100%;
+}
 </style>
