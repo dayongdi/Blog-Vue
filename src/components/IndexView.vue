93a8639439674cd678dec8aca04b54674fd4fc47
LAPTOP-B8SHAVIK\1 <328069454@qq.com>
2023-11-30 17:40:01 +0800

    no message

--- src/components/IndexView.vue
+++ src/components/IndexView.vue
@@ -1,54 +1,81 @@
 <template>
 	<div class="contain">
-		<div class="bigTitle">DYD</div>
-		<div class="smallTitile">/*这里是大永弟的个人空间*/</div>
-	</div> 
-	
-	<div class="containDiv">
-		<div id="about" class="normalTitle" >关于</div>
-		<div class="divider">
-			<v-divider :thickness="5" class="border-opacity-100 divider" color="success" />
-		</div >
-		
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-		<div class="text_content">test1.......</div>
-	</div>
-	
-	<div class="containDiv_recommond">
-		<div id="recommond" class="normalTitle">推荐</div>
-		<div class="divider">
-			<v-divider :thickness="5" class="border-opacity-100 divider" color="success" />
+		<div class="topNav">
+			<v-btn icon="$vuetify"></v-btn>
+			<v-spacer></v-spacer>
+			<v-btn class="d-none d-sm-flex button"  @click="scrollTop" 	color="transparent">
+				<div class="button_text">首页</div>
+			</v-btn>
+			
+			<v-btn class="d-none d-sm-flex button"  @click="scrollAbout" color="transparent">	
+				<div class="button_text">关于</div>
+			</v-btn>
+			<v-btn class="d-none d-sm-flex button"  @click="scrollRecommond" color="transparent">
+				<div class="button_text">推荐</div>
+			</v-btn>
+			<v-btn class="d-none d-sm-flex button"  @click="scrollBoard" color="transparent">
+				<div class="button_text">未定义</div>
+			</v-btn>
 		</div>
-			
-		<v-table class="recommond_table">
-			<div class="table-item"></div>
-			<div class="table-item"></div>
-			<div class="table-item"></div>
-			<div class="table-item"></div>
-			<div class="table-item"></div>
-			<div class="table-item"></div>
-		</v-table>
-		
-	</div>
-	
-	<div class="containDiv_bottom">
-		<div id="bottom" class="normalTitle">日记</div>
-		<div class="divider">
-			<v-divider :thickness="5" class="border-opacity-100 divider" color="success" />
-		</div >
-			
+		<div class="bigTitle">DYD</div>
 
+		<div class="smallTitile">/*这里是大永弟的个人空间*/</div>
+		<v-btn
+			class="d-flex d-sm-none drawable"
+			color="primary"
+			icon="$vuetify"
+			@click.stop="drawer = !drawer"
+		  />
+	</div>	
+	
+	
+	<div class="containDiv" @click.stop="drawer = !drawer">
+		<div class="containDiv_about" id="about">
+			<div class="normalTitle" >关于</div>
+			<v-divider :thickness="5" class="border-opacity-100 divider" color="success" />
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+			<div class="text_content">test1.......</div>
+		</div>
+		
+		<div class="containDiv_recommond" id="recommond" @click.stop="drawer = !drawer">
+			<div class="normalTitle" >推荐</div>
+			<v-divider :thickness="5" class="border-opacity-100 divider" color="success" />
+			
+			<div class="recommond_list">
+				<div class="list-item"></div>
+				<div class="list-item"></div>
+				<div class="list-item"></div>
+				<div class="list-item"></div>
+			</div>
+		</div>
+		
+		<div class="containDiv_bottom" id="bottom" @click.stop="drawer = !drawer">
+			<div class="normalTitle" >未定义</div>
+			<v-divider :thickness="5" class="border-opacity-100 divider" color="success" />
+			
+		</div>
 	</div>
 	
+
 	
+
+
+	<v-navigation-drawer v-model="drawer" location="right" temporary>
+		<v-list>
+			<v-list-item><v-btn @click="scrollTop" class="dialogItem">首页</v-btn></v-list-item>
+			<v-list-item><v-btn @click="scrollAbout" class="dialogItem">关于</v-btn></v-list-item>
+			<v-list-item><v-btn @click="scrollRecommond" class="dialogItem">日志</v-btn></v-list-item>
+			<v-list-item><v-btn @click="scrollBoard" class="dialogItem">未定义</v-btn></v-list-item>
+		</v-list>
+	</v-navigation-drawer>
 
 	
 </template>
